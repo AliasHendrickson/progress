7f992dac658af777c898c8af18b431ee3512b618
David Bicanic <dbicanic77@gmail.com>
2016-06-24 16:15:42 -0500

    Create rating controller routes no auth

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,41 @@
 class RatingsController < ApplicationController
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    @rating = Rating.new
+
+    if @rating.save
+      redirect_to #somewhere react? probably the main page
+    else
+      #This might be different with react
+      render "new"
+    end
+  end
+
+  def edit
+    @rating = find_rating
+  end
+
+  def update
+    @goal = find_goal
+
+    if @goal.update(goal_params)
+      redirect_to #somewhere react? probably the main page
+    else
+      #this may be different with react
+      render "edit"
+    end
+  end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:score, :comment)
+
+  def find_rating
+    Rating.find(params[:id])
+  end
+
 end
