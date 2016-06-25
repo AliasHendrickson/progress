2db35601c30039babf74adad3f38500b3463cc9a
Alec Hendrickson <email": null>
2016-06-25 10:19:00 -0500

    Add experience field to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624194824) do
+ActiveRecord::Schema.define(version: 20160625151359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160624194824) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar_url"
+    t.float    "experience"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
