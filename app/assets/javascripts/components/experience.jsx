97e5268b717febfd8187e0873b12eb9308e56b1a
Alec Hendrickson <email": null>
2016-06-25 14:56:31 -0500

    Display users experience value with react component

--- app/assets/javascripts/components/experience.jsx
+++ app/assets/javascripts/components/experience.jsx
@@ -1,8 +1,9 @@
 var Experience = React.createClass({
   render: function() {
+  console.log(this.props.data)
     return (
       <div className="experience">
-        <h2 className="title">Experience: </h2>
+        <h2 className="title">Experience: { this.props.data }</h2>
       </div>
     );
   }
