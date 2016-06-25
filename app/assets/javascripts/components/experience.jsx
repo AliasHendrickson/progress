058fe4391073996f5bdc9ff1d41660429a26079a
Alec Hendrickson <email": null>
2016-06-25 14:21:20 -0500

    Switch from coffeescript to JSX

---
+++ app/assets/javascripts/components/experience.jsx
@@ -0,0 +1,9 @@
+var Experience = React.createClass({
+  render: function() {
+    return (
+      <div className="experience">
+        <h2 className="title">Experience: </h2>
+      </div>
+    );
+  }
+});
