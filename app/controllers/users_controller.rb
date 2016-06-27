5d13fba61d16649d6495a65ffc69f27906d1c9ee
Theron Cross <theroncross@gmail.com>
2016-06-27 09:06:22 -0500

    Update user form for editing username and avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user = current_user
   end
 
   # POST /users
@@ -88,6 +89,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation, :username, :avatar_url)
     end
 end
