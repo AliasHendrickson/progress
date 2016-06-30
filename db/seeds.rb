6206fbba1ef231b05965735dd4c2689276aad997
AliasHendrickson <alecbh517@gmail.com>
2016-06-30 16:20:17 -0500

    Add seeds of user and goals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 person = User.create(username: "david", password: "password", password_confirmation: "password", avatar_url: "http://nation.com.pk/print_images/large/2014-04-23/1398268898-9179.jpg", email: "david@email.com", experience: 100)
-Goal.create(description: "Comb hair for presentation", duration: "daily")
+
+person.goals.create!(description: "Comb hair for presentation", duration: "daily", completed: "false", due_date: "2016-07-31 00:00:00")
+
+person.goals.create!(description: "Give pair feedback after every day", duration: "daily", completed: "false", due_date: "2016-07-31 00:00:00")
+
+person.goals.create!(description: "Journal about the joys of Nokogiri", duration: "weekly", completed: "false", due_date: "2016-07-31 00:00:00")
+
+person.goals.create!(description: "Make it through DBC without losing my cool", duration: "phase", completed: "false", due_date: "2016-07-31 00:00:00")
