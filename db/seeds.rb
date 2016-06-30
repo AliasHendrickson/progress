d5ed0c329b5305718f41cd1c2690f49f30d88606
AliasHendrickson <alecbh517@gmail.com>
2016-06-30 18:32:13 -0500

    Change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 Goal.create!(user_id: 1, completed: true, description: "Be able to add a simple, concise React Component to my app without copy/pasting", due_date: Date.new, duration: "daily")
 Goal.create!(user_id: 1, completed: true, description: "Have a killer presentation", due_date: Date.new, duration: "weekly")
-Goal.create!(user_id: 1, completed: true, description: "Graduate with my head held high", due_date: Date.new, duration: "phase")
-Goal.create!(user_id: 1, completed: true, description: "Do a thing", due_date: Date.new, duration: "daily")
+Goal.create!(user_id: 1, completed: true, description: "Build a beautiful web app by the end of my time at DBC", due_date: Date.new, duration: "phase")
 Rating.create!(goal_id: 1, score: 3)
